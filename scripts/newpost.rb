a395ed215766f5f80c1866352e506dedc854ff6c
Rohan Jain <crodjer@gmail.com>
2011-06-16 22:05:50 +0530

    Comments; Google +1

--- scripts/newpost.rb
+++ scripts/newpost.rb
@@ -1,29 +1,47 @@
 #!/usr/bin/env ruby
-
 unless ARGV[0]
   puts "Usage: newpost post-title"
   exit(-1)
 end
 
-date_prefix = Time.now.strftime("%Y-%m-%d")
-postname = ARGV[0].strip.downcase.gsub(/ /, '-').gsub('(', '-').gsub(')', '-')
+SITE_DIR = "/home/rohan/workspace/site/"
+
+def slugify(title)
+    str = title.dup
+    str.gsub!(/[^a-zA-Z0-9 ]/,"")
+    str.gsub!(/[ ]+/," ")
+    str.gsub!(/ /,"-")
+    str.downcase!
+    str
+end
+
+
+title = ARGV.join(' ')
+slug = slugify(title)
+prefix = Time.new.strftime("%Y-%m-%d")
+filename = "#{prefix}-#{slug}.mkd"
+path = File.join(SITE_DIR, "/_posts/#{filename}")
 
 header = <<-END
 ---
 layout: post
-title: "#{ARGV[0]}"
+title: "#{title}"
+slug: "#{slug}"
 published: false
 tags:
     - tag 1
     - tag 2
+categories:
+    - category 1
+    - category 2
 author: crodjer
 ---
 
 END
 
-f = File.open("/home/rohan/workspace/site/_posts/#{date_prefix}-#{postname}.mkd", 'w+')
-f << header
+f = File.open(path, 'w')
+f.write(header)
 f.close
 
-system("e #{f.path}")
+system("e #{path}")
 exit(0)
