56aa3a5621350856f068e1d4c19a3b69e8b21cb9
Rohan Jain <crodjer@gmail.com>
2012-04-01 22:34:11 +0530

    Fix regenerator script. Build site first

--- scripts/regenerate.sh
+++ scripts/regenerate.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
-SITE_DIR=${1:-$("mktemp" -d /tmp/_site.XXXXXXXXX)}
 
 cd $PROJECT_DIR
 
+ghc --make $PROJECT_DIR/site.hs
 $PROJECT_DIR/site rebuild
 $PROJECT_DIR/scripts/cleanUrls.sh
-mv _site/* $SITE_DIR/
