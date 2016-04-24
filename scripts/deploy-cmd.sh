2d09e747cd9003f753c30c9f7a4d2183a1822e18
Rohan Jain <crodjer@gmail.com>
2016-04-24 19:45:13 +0530

    Push to master as well while deploying

--- scripts/deploy-cmd.sh
+++ scripts/deploy-cmd.sh
@@ -4,4 +4,5 @@ project_root=$(git rev-parse --show-toplevel)
 export GIT_DEPLOY_DIR=$project_root/_site
 
 $project_root/scripts/deploy.sh && \
+    git push origin master && \
     git push origin gh-pages
